843cd20f2c9aeb6b60b4c0698803e1fe923b1f5b
Arxyma <ilham.arsyam@gmail.com>
2022-05-27 16:00:56 +0700

    tambah fungsi return

--- fungsi_parameter.py
+++ fungsi_parameter.py
@@ -7,4 +7,15 @@ selamatDatang('hitam', 'siang')
 selamatDatang('putih', 'sore')
 selamatDatang('ujang', 'malam')
 
-selamatDatang(waktu='siang', nama='udin')
+# selamatDatang(waktu='siang', nama='udin')
+selamatDatang = ('siang', 'udin')
+
+
+def penjumlahan(a, b):
+    hasil = a+b
+    return hasil
+
+
+hasil_jumlah = penjumlahan(2, 5)
+print(selamatDatang)
+print(hasil_jumlah)
